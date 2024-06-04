8c55c765bceabeac0c5262b5c0152aa7bd0bd640
NicolaMGH <va3nky@hotmail.com>
2024-06-04 00:48:25 -0400

    implemented randomstringgenerator

--- express_server.js
+++ express_server.js
@@ -10,6 +10,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < 6) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
