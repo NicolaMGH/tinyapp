02f01574a0f884d1d5c5ed8715878fcc0bf4a450
NicolaMGH <va3nky@hotmail.com>
2024-06-04 01:02:35 -0400

    implemented express code to save to database

--- express_server.js
+++ express_server.js
@@ -52,7 +52,13 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// app.post("/urls", (req, res) => {
+//   console.log(req.body); // Log the POST request body to the console
+//   res.send("Ok"); // Respond with 'Ok' (we will replace this)
+// });
+
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  //res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
