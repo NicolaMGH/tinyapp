4545cf9e11a0e2cd56d3c1fd85fa9ec7b1deb8cb
Nicola <nickgmetallo@gmail.com>
2024-06-09 19:23:59 -0400

    log in user after registration

--- express_server.js
+++ express_server.js
@@ -55,7 +55,7 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"],
+    user: users[req.cookies.user_id],
     urls: urlDatabase 
   };
   res.render("urls_index", templateVars);
@@ -63,13 +63,13 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"]
+    user: users[req.cookies.user_id]
   };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies.user_id] };
   res.render("urls_show", templateVars);
 });
 
@@ -92,7 +92,7 @@ app.get("/u/:id", (req, res) => {
 
 app.get("/register", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"]
+    user: users[req.cookies.user_id]
   };
   res.render("register", templateVars);
 });
@@ -113,7 +113,7 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
 
